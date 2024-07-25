47dd81f228578105a0def5014581519410af236e
Pedro H Silva <peedrohsilvasant@gmail.com>
2024-07-24 22:18:42 -0300

    separando templates

---
+++ contato.php
@@ -0,0 +1,7 @@
+<?php
+    include_once("templates/header.php")
+?>
+
+    <h1>contato</h1>
+</body>
+</html>
---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>Hora de codar &copy; 2024</p>
+</footer>
+</body>
+</html>
